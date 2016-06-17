4ee85e3ae3452b8df775bfe6bb62b096e6dade5a
Stefano De Vuono <io@stefanodevuono.com>
2016-06-16 19:06:17 -0700

    glitter

--- sketch.js
+++ sketch.js
@@ -1,46 +1,16 @@
-// for red, green, and blue color values
-var r, g, b;
-var x = 0;
-var ellipses = []
-
 function setup() {
-    createCanvas(windowWidth,windowHeight);
-  // Pick colors randomly
-  // r = random(255);
-  // g = random(255);
-  // b = random(255);
+  createCanvas(windowWidth,windowHeight);
 }
 
 function draw() {
   background(0);
-  // Draw a circle
-  // strokeWeight(2);
-  // stroke(r, g, b);
-  // fill(r, g, b, 127);
-  // ellipse(360, 200, 200, 200);
 
-  for (var x = 0; x < width; x += 50) {
-    for (var y = 0; y < height; y += 50) {
-      fill(x, 200, 255);
+  for (var x = 0; x < width; x += 30) {
+    for (var y = 0; y < height; y += 30) {
+      var glitter = random(0,255);
+      fill(190, 0, 255, glitter);
       ellipse(x, y, 40, 40);
     }
   }
-  // x++;
+
 }
-
-
-
-// function setup() {
-//   createCanvas(windowWidth,windowHeight);
-//
-// }
-// function draw() {
-//   background(0);
-//   if(mouseX<250){
-//     fill(255);
-//   }else{
-//     fill(0,200,255);
-//   }
-//
-//   ellipse(mouseX,mouseY,40,40);
-// }
